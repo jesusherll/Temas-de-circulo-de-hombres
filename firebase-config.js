99b32770205d2c3a59eea5cef3efaa48d44cf3e2
JesusHerll <jus00714@hotmail.com>
2026-07-08 20:52:45 -0600

    feat: sistema de gestion de temas con Firebase y diseno arcoiris

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC5GvS8S0OxYI7M5RGFQDpKlq2z7OUHAvA",
+    authDomain: "circulo-de-hombres-6043c.firebaseapp.com",
+    databaseURL: "https://circulo-de-hombres-6043c-default-rtdb.firebaseio.com",
+    projectId: "circulo-de-hombres-6043c",
+    storageBucket: "circulo-de-hombres-6043c.firebasestorage.app",
+    messagingSenderId: "1019405395982",
+    appId: "1:1019405395982:web:19c2a9a990df22300df45f"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
